0ad6627aaffa2823b9177fef6c63adc67d2b1b86
Bugra Senol <bsenol@k2m06s06.42kocaeli.com.tr>
2022-02-16 16:08:52 +0300

    asfsadg

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bsenol <bsenol@student.42kocaeli.com.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 23:48:03 by bsenol            #+#    #+#             */
-/*   Updated: 2022/02/10 10:01:27 by bsenol           ###   ########.tr       */
+/*   Updated: 2022/02/16 16:07:30 by bsenol           ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_atoi(const char *str)
 {
-	int					i;
-	int					x;
-	unsigned long int	res;
+	int	i;
+	int	x;
+	int	res;
 
 	i = 0;
 	x = 1;
@@ -34,9 +34,5 @@ int	ft_atoi(const char *str)
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}	
-	if (res > 9223372036854775807 && x != -1)
-		return (-1);
-	if (res > 9223372036854775807 && x == -1)
-		return (0);
-	return ((int)(res * x));
+	return (res * x);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,24 +12,55 @@
 
 #include "libft.h"
 
+static int	ft_getstart(const char *s1, const char *set)
+{
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1);
+	i = 0;
+	while (i < len)
+	{
+		if (ft_strchr(set, s1[i]) == 0)
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_getend(const char *s1, const char *set)
+{
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1);
+	i = 0;
+	while (i < len)
+	{
+		if (ft_strchr(set, s1[len - i - 1]) == 0)
+			break ;
+		i++;
+	}
+	return (len - i);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	j;
-	char	*str;
+	int		start;
+	int		end;
+	char	*newstr;
 
-	str = 0;
-	if (s1 != 0 && set != 0)
-	{
-		i = 0;
-		j = ft_strlen(s1);
-		while (s1[i] && ft_strchr(set, s1[i]))
-			i++;
-		while (s1[j - 1] && ft_strchr(set, s1[j - 1]) && j > i)
-			j--;
-		str = (char *)malloc(sizeof(char) * (j - i + 1));
-		if (str)
-			ft_strlcpy(str, &s1[i], j - i + 1);
-	}
-	return (str);
+	if (s1 == NULL)
+		return (NULL);
+	if (set == NULL)
+		return (ft_strdup(s1));
+	start = ft_getstart(s1, set);
+	end = ft_getend(s1, set);
+	if (start >= end)
+		return (ft_strdup(""));
+	newstr = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (newstr == NULL)
+		return (NULL);
+	ft_strlcpy(newstr, s1 + start, end - start + 1);
+	return (newstr);
 }
